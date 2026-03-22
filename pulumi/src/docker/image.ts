832965f7fa8c267e8dbd3ed541662b1a66b7264f
Andrew Meier <ameier38@gmail.com>
2026-03-22 11:45:46 -0400

    clean up

--- pulumi/src/docker/image.ts
+++ pulumi/src/docker/image.ts
@@ -4,9 +4,12 @@ import * as path from 'path'
 import * as config from '../config'
 import { provider } from './provider'
 
+const registryUri = config.dockerConfig.registryUri
+const registryHost = registryUri.split('/')[0]
+
 export const image = new dockerBuild.Image(config.identifier, {
     tags: [
-        pulumi.interpolate`${config.dockerConfig.registryUri}/${config.identifier}`
+        pulumi.interpolate`${registryUri}/${config.identifier}`
     ],
     context: {
         location: path.join(config.rootDir, 'app')
@@ -15,6 +18,11 @@ export const image = new dockerBuild.Image(config.identifier, {
         dockerBuild.Platform.Linux_amd64
     ],
     push: true,
+    registries: [{
+        address: registryHost,
+        username: 'oauth2accesstoken',
+        password: config.dockerConfig.registryAccessToken,
+    }],
 }, { provider })
 
 export const imageRef = image.ref
